8691a218ba44cf359313c84d39906bc2771983d2
ggoodale <ggoodale@gmail.com>
2010-03-30 15:19:05 -0700

    Removed future-looking files

--- lib/wearehunted.rb
+++ lib/wearehunted.rb
@@ -134,9 +134,3 @@ module WeAreHunted
     end * '&'
   end
 end
-
-directory = File.expand_path(File.dirname(__FILE__))
-
-# require File.join(directory, "wearehunted", "suggest")
-# require File.join(directory, "wearehunted", "chart")
-# require File.join(directory, "wearehunted", "lookup")
